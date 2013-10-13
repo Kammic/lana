42139625ba72f75fde55386f35047eff592a6ad8
Sumeet Singh <ortuna@gmail.com>
2013-10-13 12:22:45 -0400

    More tests

--- lib/lana/manifest.rb
+++ lib/lana/manifest.rb
@@ -1,5 +1,19 @@
-module Lana
-  module Manifest
+require 'yaml'
 
+module Lana
+  class FileNotFound < Exception; end
+
+  class Manifest
+    attr_reader :path
+
+    def initialize(path)
+      raise FileNotFound unless File.exists? path
+      @path = path
+    end
+
+    private
+    def digest(path)
+      YAML.load_file path
+    end
   end
 end
