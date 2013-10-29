8d384c7a9759cd76127101210b50987a0c0ebc83
Sumeet Singh <ortuna@gmail.com>
2013-10-29 01:28:53 -0500

    Working compiler

--- lib/lana/compiler.rb
+++ lib/lana/compiler.rb
@@ -1,7 +1,29 @@
 module Lana
   class Compiler
     def initialize(manifest)
-      
+      @manifest = manifest
     end
+
+    def compile(output_path)
+      system("pandoc -o #{output_path} #{page_list.join(' ')}")
+    end
+
+    private
+    def page_list
+      recursive_page_list(@manifest["pages"]).flatten
+    end
+
+    def recursive_page_list(manifest_pages)
+      [].tap do |pages|
+        manifest_pages.each do |_, value|
+          if value.respond_to? :each
+            pages << recursive_page_list(value)
+          else
+            pages << value
+          end
+        end
+      end
+    end
+
   end
 end
