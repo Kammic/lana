bfcb0ae5df9bd51a36ccf2e86033f92f985cac26
Sumeet Singh <ortuna@gmail.com>
2013-10-29 20:56:45 -0500

    Working compiler

--- lib/lana/compiler.rb
+++ lib/lana/compiler.rb
@@ -1,12 +1,26 @@
 module Lana
   class Compiler
-    def initialize(manifest)
-      @manifest = manifest
+    attr_reader :base_path
+
+    def initialize(manifest, base_path = '')
+      @manifest   = manifest
+      @base_path  = add_slash(base_path)
     end
 
     def compile(output_path)
-      pages = @manifest.pages
+      pages = @manifest.pages.map { |page| full_path(page) }
       system("pandoc -o #{output_path} #{pages.join(' ')}")
+    end
+
+    private
+    def add_slash(path)
+      return path if path == ''
+      return path if path.end_with?('/')
+      "#{path}/"
+    end
+
+    def full_path(file)
+      "#{@base_path}#{file}"
     end
   end
 end
--- spec/lana/compiler_spec.rb
+++ spec/lana/compiler_spec.rb
@@ -7,10 +7,40 @@ describe Lana::Compiler do
     @manifest = Lana::Manifest.new(fixture_path)
     @compiler = subject.new(@manifest)
   end
+  context 'book' do
+    it 'can compile a book' do
+      output_path = File.expand_path('../../tmp/output.pdf', __FILE__)
+      base_path   = File.expand_path('../../fixtures/example_book', __FILE__)
+      manifest    = Lana::Manifest.new(fixture_path('simple.json'))
+      compiler    = subject.new(manifest, base_path)
+      compiler.compile(output_path)
+      expect(File.exists?(output_path)).to eq(true)
+      FileUtils.rm(output_path)
+    end
+  end
 
   context '#initialize' do
     it 'can take manifest as an argument' do
       subject.new(@manifest)
+    end
+
+    it 'takes a base path' do
+      manifest = Lana::Manifest.new(fixture_path('simple.json'))
+      compiler = subject.new(manifest, 'example/')
+    end
+  end
+
+  context '#full_path' do
+    before do
+      @simple_manifest = Lana::Manifest.new(fixture_path('simple.json'))
+    end
+
+    it 'creates a path relative to base_path' do
+      compiler = subject.new(@simple_manifest, 'example/')
+      expect(compiler.send(:full_path,'main.md')).to eq('example/main.md')
+
+      compiler = subject.new(@simple_manifest, 'example')
+      expect(compiler.send(:full_path,'main.md')).to eq('example/main.md')
     end
   end
 
@@ -19,6 +49,15 @@ describe Lana::Compiler do
       pages = @manifest.pages
       @compiler.should_receive(:system).with("pandoc -o output.pdf #{pages.join(' ')}")
       @compiler.compile("output.pdf")
+    end
+
+    it 'sends the pages list with a base_path to pandoc' do
+      manifest = Lana::Manifest.new(fixture_path('simple.json'))
+      pages    = manifest.pages.map { |page| "example/#{page}" }
+      compiler = subject.new(manifest, 'example/')
+
+      compiler.should_receive(:system).with("pandoc -o output.pdf #{pages.join(' ')}")
+      compiler.compile("output.pdf")
     end
   end
 end
